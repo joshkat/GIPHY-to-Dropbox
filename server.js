6933e02900c1a84d4fddb7fcf549e21afe1c99ac
joshkat <joshua02k@gmail.com>
2023-05-09 11:50:27 -0400

    code cleanup

--- server.js
+++ server.js
@@ -32,20 +32,15 @@ function connection_handler(req, res){
         const state = crypto.randomBytes(20).toString("hex");
         const searched_gif = inputURL.searchParams.get("gif");
         task_states.push({state, searched_gif});
-
-        console.log(searched_gif);
         giphy_request({response: res, searched_gif: searched_gif, state: state});
-
     }else if(req.url.startsWith("/received_code")){
         const url = new URL(req.url, "http://localhost:3000/");
         const code = url.searchParams.get("code");
-        console.log(code);
         const state = url.searchParams.get("state");
 
         //when auth is granted (no error) proceed
         if(!url.searchParams.get("error")){
             let task_state = task_states.find(task_state => task_state.state === state);
-            console.log(task_state);
             if(code === undefined || state === undefined || task_state === undefined){
                 bad_request_error(res);
                 return;
@@ -157,14 +152,13 @@ function upload_to_dropbox(stringJSON, gif_dir, response){
         }    
     };
     fs.readFile(`${gif_dir}.gif`, (err, data) => {
-        console.log(gif_dir);
         if(err){
             console.error(err);
             return;
         }
         const upload_request = https.request(upload_endpoint, options, (upload_response) => { stream_to_message(upload_response, receive_upload_response, response, gif_dir) })
             .end(data);
-    })
+    });
 }
 
 function receive_upload_response(body, response, gif_dir){
@@ -183,7 +177,7 @@ function receive_upload_response(body, response, gif_dir){
 }
 
 function bad_request_error(response){
-    response.writeHead(302, {Location: "./html/error400.html"});
+    response.writeHead(302, {Location: "./html/error404.html"});
     response.end();
 }
 
